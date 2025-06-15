17d789ae321f34d82da260fc558471ff0fd9b15e
Elizeu Brito <elizeu.brito@gmail.com>
2025-06-15 15:25:40 -0300

    Adding ci pipeline

--- src/test/java/br/com/qam/TestGoogle.java
+++ src/test/java/br/com/qam/TestGoogle.java
@@ -3,16 +3,32 @@ package br.com.qam;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class TestGoogle {
 
     @Test
-    public void googleSearch() {
+    public void googleSearch() throws MalformedURLException, InterruptedException {
 
-        WebDriver driver = new ChromeDriver();
+        URL seleniumUrl = new URL("http://localhost:4444/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+
+        WebDriver driver = new RemoteWebDriver(seleniumUrl, capabilities);
+
         driver.get("https://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("OpenAI ChatGPT");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("OpenAI ChatGPT");
+        searchBox.submit();
+
+        Thread.sleep(3000);
+
         driver.quit();
+
     }
 }
