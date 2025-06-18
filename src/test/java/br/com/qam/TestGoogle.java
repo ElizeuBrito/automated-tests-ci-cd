df05f3926c40cc10f78868637e2c9723c5211d3b
Elizeu Brito <elizeu.brito@gmail.com>
2025-06-17 22:50:21 -0300

    feature: Adding Allure report to be sent by e-mail and be available through github pages server

--- src/test/java/br/com/qam/TestGoogle.java
+++ src/test/java/br/com/qam/TestGoogle.java
@@ -1,5 +1,10 @@
 package br.com.qam;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,9 +15,14 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+@Epic("Google search engine")
+@Feature("Searches passing criteria")
+@Story("Search with success")
+@DisplayName("Valid search with criteria test")
 public class TestGoogle {
 
     @Test
+    @Description("Tests if the search was successfully")
     public void googleSearch() throws MalformedURLException, InterruptedException {
 
         URL seleniumUrl = new URL("http://localhost:4444/wd/hub");
